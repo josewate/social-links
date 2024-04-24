80381f0a2e6c634a46af89139d574d7bfdc73d5b
José Wate <52595409+josewate@users.noreply.github.com>
2024-04-24 17:19:14 +0200

    :tada: initial commit

---
+++ script.js
@@ -0,0 +1,18 @@
+function toggleMode() {
+    const html = document.documentElement
+    html.classList.toggle("light")
+
+    // Pegar a tag img
+    const img = document.querySelector("#profile img")
+
+    // Substituir a imagem
+    if(html.classList.contains('light')) { 
+        // Se tiver light mode, adicionar a imagem light
+        img.setAttribute("src", "./assets/avatar-light.png")
+        img.setAttribute("alt", "Foto de maike Brito sorrindo, usando oculos escuros e um casaco preto, sem barba e fundo azul roxo")
+    } else {
+        // Se tiver sem light mode, manter a imagem normal
+        img.setAttribute("src", "./assets/avatar.png")
+        img.setAttribute("alt", "Foto de maike Brito sorrindo, usando oculos e um casaco preto, sem barba e fundo azul roxo")
+    }
+}
